599b28c7255264c7bc88f2cb0e1881b526c856c2
theovast <theovast@MacBook-Pro-de-theovast-2.local>
2020-06-27 11:35:53 +0200

    Updates

---
+++ precache-manifest.7c1e53814897da7da6a69db5559e6a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009f459cfc4318c8c1b5f35419a6267d",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "12d393b4bf190624843b",
+    "url": "/react-calculator/static/css/main.2c83cdf5.chunk.css"
+  },
+  {
+    "revision": "46514c20b3edc2e9cc5e",
+    "url": "/react-calculator/static/js/2.4ae3a3f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.4ae3a3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d393b4bf190624843b",
+    "url": "/react-calculator/static/js/main.d80c3a9d.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
